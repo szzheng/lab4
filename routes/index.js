edf6190163f283b83723ea87a5c035903c8b4c7b
Steven Zheng <szzheng@ucsd.edu>
2016-01-28 17:25:21 +0000

    node.js lab

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,20 @@
  */
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index', {
+    'projects': [
+      { 'name': 'Chore Manager',
+        'image': 'lorempixel.people.1.jpeg',
+        'id': 'project1'
+      },
+      { 'name': 'Avalon (The Resistance) Google Hangout Extension',
+        'image': 'lorempixel.city.1.jpeg',
+        'id': 'project2'
+      },
+      { 'name': 'Family Locator App',
+        'image': 'lorempixel.technics.1.jpeg',
+        'id': 'project3'
+      },
+    ]
+  });
 };
